f218ab0c923625f691f5666aa8fa5b61133183e2
Ayush Sinha <ayush_sinha@yaskawa.com>
2020-04-26 16:43:42 -0700

    Proj 04 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// prev stores KBD's value from prev iteration
+@prev
+M = 0
+
+(LOOP)
+  @KBD
+  D = M
+
+  @prev
+  D = D - M
+  @SAMECOLOR // If color doesn't need to be changed
+  D; JEQ
+
+  // If color needs to be changed
+  @KBD
+  D = M
+  @prev // save KBD value to prev
+  M = D
+  @WHITEN
+  D; JEQ
+
+  // BLACKEN
+  @pixelvalue
+  M = -1
+  @CHANGECOLOR
+  0; JMP
+
+  (WHITEN)
+    @pixelvalue
+    M = 0
+    @CHANGECOLOR
+    0; JMP
+
+  (CHANGECOLOR)
+    // for loop to set all pixels to pixelvalue
+
+    // Set iterator i to zero
+    @i
+    M = 0
+
+    // Total number of pixels numpix
+    @8192
+    D = A
+    @numpix
+    M = D
+
+    (SETSCREEN)
+      // Test if all pixels are done?
+      @i
+      D = M
+      @numpix
+      D = D - M
+      @LOOP
+      D; JEQ // All pixels done
+
+      // selected pixel selpix
+      @i
+      D = M
+      @SCREEN
+      D = D + A
+      @selpix
+      M = D
+
+      // change selected pixel's value
+      @pixelvalue
+      D = M
+      @selpix
+      A = M
+      M = D
+
+      // increment iterator i
+      @i
+      M = M + 1
+
+      // Loop back for more pixels
+      @SETSCREEN
+      0; JMP
+
+  (SAMECOLOR)
+    @LOOP
+    0; JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Init R2 to zero
+@R2
+M = 0
+
+// iterator starts at zero
+@iter
+M = 0 // iter = 0
+
+// loop to add R1 R0 times
+(LOOP)
+  @R0
+  D = M
+  @iter
+  D = M - D
+  @STOP
+  D; JEQ
+
+  // Add
+  @R1
+  D = M
+  @R2
+  M = D + M
+
+  // Increment iter
+  @iter
+  M = M + 1
+
+  // Loop again
+  @LOOP
+  0; JMP
+
+// Stop
+(STOP)
+  @STOP
+  0; JMP
